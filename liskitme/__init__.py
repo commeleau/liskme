91eca70aeea1ead5d747d05711ac5596924ee62f
gab88slash <consigliog@gmail.com>
2016-06-01 00:09:41 +0200

    removed ming from liskit init package

--- liskitme/__init__.py
+++ liskitme/__init__.py
@@ -1,15 +1,8 @@
 from liskitme.model import init_model
-from ming.config import configure
-from ming.datastore import create_datastore
-from ming.odm.odmsession import ThreadLocalODMSession
+
 from sqlalchemy.engine import create_engine
 
 base_timestamp = 1428544800
-
-
-configure(**{'ming.mysession.uri': 'mongodb://localhost:27017/tutorial'})
-bind = create_datastore('tutorial')
-session = ThreadLocalODMSession.by_name('mysession')
 
 # engine and delegate are temporarily hard coded TODO: remove them and place in config
 engine = create_engine('sqlite:///blockchain-last.db', echo=True)
