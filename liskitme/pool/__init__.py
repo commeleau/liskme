82fcedcc3000a1864c5b52fa3fcbfa5f46f97a4e
gab88slash <consigliog@gmail.com>
2016-06-02 19:56:40 +0200

    refactor of init model

--- liskitme/pool/__init__.py
+++ liskitme/pool/__init__.py
@@ -2,6 +2,6 @@ from session import mainsession, DBSession
 from mongoengine import connect
 
 
-def init_model(database, host, port, username, password):
+def init_model(database, host="localhost", port=27017, username="", password=""):
     """Call me before using any of the tables or classes in the model."""
     connect(database, host=host, port=port, username=username, password=password)
