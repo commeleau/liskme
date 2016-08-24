bcf43d784f6621f7634fbf9f4fbf6aef9c094cb2
gab88slash <consigliog@gmail.com>
2016-08-24 12:03:06 +0200

    fix in init

--- liskitme/schedule/__init__.py
+++ liskitme/schedule/__init__.py
@@ -16,7 +16,6 @@ delegate = config.get('main', 'delegate')
 """
 Init of sql database PostGres Soon
 """
-# engine and delegate are temporarily hard coded TODO: remove them and place in config
 engine = create_engine(config.get('chain', 'db_url'), echo=True)
 init_sqlalchemy_model(engine)
 
@@ -26,5 +25,5 @@ Init of mongo database
 # bind = create_datastore('mongodb://localhost:27017/lisk-pool')
 init_mongo_model(
     config.get('mongo', 'db_name'),
-    host=config.get('mongo', 'host'), port=config.get('mongo', 'port'),
+    host=config.get('mongo', 'host'), port=int(config.get('mongo', 'port')),
     username=config.get('mongo', 'username'), password=config.get('mongo', 'password'))
